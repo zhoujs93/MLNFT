58e8070c91a5aa33c4bb23d537b14f514a936ba6
zhoujs93 <john23@berkeley.edu>
2022-04-04 01:21:40 -0700

    remove function

--- requestData.py
+++ requestData.py
@@ -7,8 +7,8 @@ import pickle, subprocess
 from collections import defaultdict
 from collections import deque
 # Get an API key pair for free here: https://dashboard.blockchainapi.com/
-MY_API_KEY_ID = ""
-MY_API_SECRET_KEY = ""
+MY_API_KEY_ID = "VAG3eU6AZLbTjm9"
+MY_API_SECRET_KEY = "MryAXR7kl3oYEPG"
 
 BLOCKCHAIN_API_RESOURCE = TheBlockchainAPIResource(
     api_key_id=MY_API_KEY_ID,
@@ -57,6 +57,20 @@ def get_closed_CM(public_key):
         print(f"Transaction Signature: {transaction}")
     return transactions
 
+def get_hashlist(cm_id, base_dir, is_v2 = True):
+    rpc = 'https://polished-cool-surf.solana-mainnet.quiknode.pro/66bc31e049b46dd293092114e2d8717b3378033f/'
+    t = '180'
+    if is_v2:
+        cmd = ['metaboss', 'snapshot', 'mints', '-c', cm_id, '-r', rpc, '-t', t, '--output', './', '--v2']
+    else:
+        cmd = ['metaboss', 'snapshot', 'mints', '-c', cm_id, '-r', rpc, '-t', t, '--output', './']
+    p = subprocess.run(cmd, cwd=str(base_dir / 'hashlist'))
+    filename = f'{cm_id}_mint_accounts.json'
+    dir_hlist = base_dir / 'hashlist'
+    with open(str(dir_hlist / filename), 'r') as file:
+        hlist = json.load(file)
+    return hlist
+
 
 if __name__ == '__main__':
     # load all candy machines
@@ -74,9 +88,11 @@ if __name__ == '__main__':
                 pickle.dump(result, file)
 
     # try to retrieve the hashlist of every nft
+    me_data = {}
     cm_v2 = ['Taiyo Robotics', 'Mindfolk', 'Boryoku Dragonz', 'Degods', 'Famous Fox',
              'Portals', 'Lfinity Flames', 'Catalina', 'Degen Ape Academy',
-             'Aurory', 'Galactic Geckos', 'Nyan Heroes', 'Stoned Ape Club']
+             'Aurory', 'Galactic Geckos', 'Nyan Heroes', 'Stoned Ape Club',
+             'Male HODL Whales']
     results = []
     index = 0
     try:
@@ -95,28 +111,53 @@ if __name__ == '__main__':
         name = collection['name']
         data[name] = collection
 
+    me_data['magiceden_project_data'] = data
+
     sample = {}
     for k, v in data.items():
         for name in cm_v2:
             if name in k:
                 sample[name] = v
 
-    tx_data = defaultdict(list)
+    listings = defaultdict(list)
+    index = 0
     for k, v in sample.items():
         symbol = v['symbol']
         print(f'Moving to {symbol}')
-        try:
-            index = 0
-            results = []
-            while True:
-                if index % 100 == 0:
-                    print(f'{symbol} : {index}')
-                time.sleep(5)
-                result = requests.get(url = f'https://api-mainnet.magiceden.dev/v2/collections/{symbol}/'
-                                            f'listings?offset={index}&limit=100')
-                result = result.json()
-                tx_data[symbol] += result
-                index += 100
-        except Exception as e:
-            print(f'{index} : {e}')
 
+        result = requests.get(url = f'https://api-mainnet.magiceden.dev/v2/collections/{symbol}/listings?offset={index}&limit=5')
+        result = result.json()
+        listings[symbol] += result
+
+    me_data['test_listings_data'] = listings
+
+    whales = listings['solana_hodl_whales']
+    pprint.pprint(whales[0])
+    hashmap_cmid = {}
+    errors = set()
+    for k, v in listings.items():
+        for value in v:
+            if len(hashmap_cmid.get(k, [])) == 0:
+                try:
+                    token_address = value['tokenMint']
+                    result = BLOCKCHAIN_API_RESOURCE.get_candy_machine_id_from_nft(
+                        mint_address=token_address,
+                        network=SolanaNetwork.MAINNET_BETA
+                    )
+                    hashmap_cmid[k] = result
+                except Exception as e:
+                    print(f'Error for {k} : {e}')
+                    errors.add(k)
+            else:
+                break
+    # add test data
+    me_data['sample_cm_id'] = hashmap_cmid
+    cmid_to_hashlist = {}
+    # get hashlist of CMID
+    for key, val in hashmap_cmid.items():
+        cm_id = val['candy_machine_id']
+        is_v2 = val['candy_machine_contract_version'] == 'v2'
+        hashlist = get_hashlist(cm_id, base_dir, is_v2 = is_v2)
+        cmid_to_hashlist[key] = hashlist
+
+    
